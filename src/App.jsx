a9bab760d59f67f176b593168bf2d7aa1d96badd
faisal-din <dahrifaisal56@gmail.com>
2024-09-28 15:34:43 +0500

    services section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benifits from './Components/Benifits';
 import Collaboration from './Components/Collaboration';
 import Header from './Components/Header';
 import Hero from './Components/Hero';
+import Services from './Components/Services';
 import ButtonGradient from './assets/svg/ButtonGradient';
 
 function App() {
@@ -11,9 +12,9 @@ function App() {
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
         <Hero />
-
         <Benifits />
         <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
